e609fc71d5f9b8db4d4990d7f13389071d14b4b0
Peter Baggetta <petebaggetta@gmail.com>
2025-08-31 20:12:30 -0400

    - Added Circle as a floor shape - Cleaned up code if statements for all shapes for a more linear and better code flow - Changed the questions asked for rectangle, triangle and circle so it does not all say width and height. Triangle asks for base and height. Circle asks for radius. - Cleaned up code for the totalFloorCost and laborCost as they were very similar. - Created an area variable

--- TileCostCalculator/Program.cs
+++ TileCostCalculator/Program.cs
@@ -6,45 +6,70 @@
         {
             const int AVERAGE_FLOORING_PER_HOUR = 20;
             const double FLOORING_COST_PER_HOUR = 86.00;
+            const string RECTANGLE_UPPERCASE = "Rectangle";
+            const string RECTANGLE_LOWERCASE = "rectangle";
+            const string TRIANGLE_UPPERCASE = "Triangle";
+            const string TRIANGLE_LOWERCASE = "triangle";
+            const string CIRCLE_UPPERCASE = "Circle";
+            const string CIRCLE_LOWERCASE = "circle";
 
-            double totalFloorCost, laborCost;
+
+            double area = 0, totalFloorCost, laborCost;
 
             //Introduction Text and ask for width (using \n character to separate)
-            Console.WriteLine("Welcome to Tile Cost Calculator.\nThis will help you calculate how much it would cost to cover a specified area with flooring.\n\nWhat shape is your floor, Rectangle or Triangle? (Choose 1): ");
+            Console.WriteLine("Welcome to Tile Cost Calculator.\nThis will help you calculate how much it would cost to cover a specified area with flooring.\n\nWhat shape is your floor? Rectangle, Triangle or Circle? (Choose 1): ");
 
             string floorShape = Console.ReadLine();
 
-            if (floorShape != "Rectangle" && floorShape != "Triangle" && floorShape != "rectangle" && floorShape != "triangle")
+            if (floorShape != RECTANGLE_UPPERCASE && floorShape != TRIANGLE_UPPERCASE && floorShape != RECTANGLE_LOWERCASE && floorShape != TRIANGLE_LOWERCASE && floorShape != CIRCLE_UPPERCASE && floorShape != CIRCLE_LOWERCASE)
             {
-                Console.WriteLine("The given shape is not supported, please choose either Rectangle or Triangle.");
-                Environment.Exit(0);
+                Console.WriteLine("The given shape is not supported, please choose either Rectangle, Triangle or Circle.");
+                return;
             }
 
+            if (floorShape == RECTANGLE_UPPERCASE || floorShape == RECTANGLE_LOWERCASE)
+            {
+                //Ask for floor width
+                Console.WriteLine("\nPlease enter the floor width: ");
+                double floorWidth = Convert.ToDouble(Console.ReadLine());
 
-            //Ask for floor width
-            Console.WriteLine("\nPlease enter the floor width: ");
-            double floorWidth = Convert.ToDouble(Console.ReadLine());
+                //Ask for floor length
+                Console.WriteLine("\nPlease enter the floor length: ");
+                double floorHeight = Convert.ToDouble(Console.ReadLine());
 
-            //Ask for floor length
-            Console.WriteLine("\nPlease enter the floor length: ");
-            double floorHeight = Convert.ToDouble(Console.ReadLine());
+                // Calculate area
+                area = floorWidth * floorHeight;
+            }
+            if (floorShape == TRIANGLE_UPPERCASE || floorShape == TRIANGLE_LOWERCASE)
+            {
+                //Ask for floor base
+                Console.WriteLine("\nPlease enter the floor base: ");
+                double floorBase = Convert.ToDouble(Console.ReadLine());
+
+                //Ask for floor height
+                Console.WriteLine("\nPlease enter the floor height: ");
+                double floorHeight = Convert.ToDouble(Console.ReadLine());
+
+                // Calculate area
+                area = (floorBase * floorHeight) / 2.0;
+
+            }
+            else // Floor is a circle
+            {
+                //Ask for floor radius
+                Console.WriteLine("\nPlease enter the floor radius: ");
+                double floorRadius = Convert.ToDouble(Console.ReadLine());
+
+                area = Math.PI * floorRadius * floorRadius;
+            }
 
             //Ask for floor cost per 1 unit
             Console.WriteLine("\nPlease enter the cost per 1 unit flooring: ");
             double floorCost = Convert.ToDouble(Console.ReadLine());
 
-            if (floorShape == "Rectangle" || floorShape == "rectangle")
-            {
-                //Calculate total cost of the square floor and the labor
-                totalFloorCost = floorWidth * floorHeight * floorCost;
-                laborCost = ((floorWidth * floorHeight) / AVERAGE_FLOORING_PER_HOUR) * FLOORING_COST_PER_HOUR;
-            }
-            else
-            {
-                //Calculate total cost of the triangle floor and the labor
-                totalFloorCost = ((floorWidth * floorHeight) / 2) * floorCost;
-                laborCost = (((floorWidth * floorHeight) / 2) / AVERAGE_FLOORING_PER_HOUR) * FLOORING_COST_PER_HOUR;
-            }
+            // Calculate the cost of the floor and labor
+            totalFloorCost = area * floorCost;
+            laborCost = (area / AVERAGE_FLOORING_PER_HOUR) * FLOORING_COST_PER_HOUR;
 
             //Output result
             Console.WriteLine($"\nThe flooring will cost you ${Math.Round(totalFloorCost, 2)}.\nThe labor will cost you ${Math.Round(laborCost, 2)}.");
